d21d8e371123f917818a7bf6766d5a32c5a4fe79
student <student@PATC21.local>
2019-03-27 12:33:17 -0700

    first

---
+++ app.js
@@ -0,0 +1,128 @@
+// function1()
+// console.log('we finished');
+// console.log('at the end');
+
+// let myObject ={
+//   myValue1: 10,
+//   someOtherValue: 20,
+//   val3: 30,
+//   val4: 40,
+//   val5:50,
+//
+//   sayHello: function () {
+//     console.log('hello');
+//   }
+// };
+//
+// let myArray = [10,20,30];
+//
+// myObject['myValue1'];
+// myObject['someOtherValue'];
+//
+// myObject.sayHello();
+//
+// myObject.myValue1
+//
+// console.log(myObject.myValue1)
+
+// let person1 = ['Eduardo','Garcia', '123', 'Cool St', 'San Francisco', 'CA', '94110'];
+//
+// let currentar = {
+//   firstName: 'Eduardo',
+//   lastName: 'Garcia',
+//   aptNumber: '123',
+//   street: 'Cool St.',
+//   city: 'San Francisco',
+//   state: 'CA',
+//   zip: '94113'
+// };
+//
+// console.log(person2.lastName);
+
+let currentCar = {
+  model: "Ford",
+  licensePLates: 'QNP444',
+  engine: '5.3',
+  color: 'red',
+  wheelSize: 24
+}
+
+console.log(currentCar.model);
+
+
+let name = 'Francisco';
+
+function myName(name) {
+
+  console.log(name);
+
+};
+myName(name)
+
+
+
+// function1()
+// console.log('we finished');
+// console.log('at the end');
+//
+//
+// function function1() {
+//   function2();
+//   function3();
+// }
+//
+// function function2() {
+//   console.log('function 2 ran');
+// }
+//
+// function function3() {
+//   console.log('function 3 ran');
+// }
+
+
+
+
+// console.log('testing');
+//
+// let countdown = 10;
+//
+// function1();
+//
+// for (let i =0; i<10; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, 3000);
+// }
+//
+// function function1() {
+//   console.log('function 1 ran');
+//
+//  if (countdown > 0) {
+//    countdown--;
+//    console.log('before ' + countdown);
+//    function1();
+//    countdown--;
+//  }
+// }
+//
+// if (num > 0) {
+//   function2(num - 1);
+// } else{
+//   return num
+//   }
+// }
+//
+// function function2() {
+//   console.log('function2 ran');
+// function2(num - 1);
+// }
+//
+// let date = 'wednesday';
+//
+// let function3 = function () {
+//   console.log(date);
+// }
+//
+// date = 'Thursday';
+//
+// function3();
